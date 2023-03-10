c6eb3a3db22b9524ec1cdc09a0f4a829cf069c77
ShakhzodUsmanov <shaxzodusmanov11@gmail.com>
2023-03-10 23:31:33 +0500

    created addIngoForm

--- src/pages/Info/index.jsx
+++ src/pages/Info/index.jsx
@@ -1,10 +1,45 @@
-import { Table } from "antd";
-import React from "react";
+import { Table, Button } from "antd";
+import React, {useState, useRef} from "react";
 import { useGetData } from "../../utils/hooks";
+import PostProductModal from "../../components/postProductModal/PostProductModal";
+import styled from "styled-components";
+import AddInfoForm from "./addInfoForm/addInfoForm";
+
+
+const Container = styled.div`
+  width: 100%;
+  padding-bottom: 30px;
+`;
+const Top = styled.div`
+  width: 100%;
+  height: 50px;
+  /* background-color: #f4f6f8; */
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 20px;
+`;
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin: 20px 0;
+`;
+
+
 
 const Info = () => {
   const infos = useGetData(["infos"], "/information");
   const item = infos?.data?.data?.[0];
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const formRef = useRef(null);
+
+  const resForm = () => {
+    setModalOpen(false);
+    formRef.current.resetFields();
+  };
+
   console.log(item);
   const dataSource = [
     {
@@ -17,9 +52,33 @@ const Info = () => {
     },
   ];
 
+  console.log(infos);
+
   return (
     <>
-      <Table dataSource={dataSource} columns={columns} />
+      <Container>
+        <Top>
+          <Button
+            type="primary"
+            size="large"
+            onClick={() => setModalOpen(true)}
+          >
+            Add Information
+          </Button>
+        </Top>
+        <InfoWrapper>
+
+          <Table dataSource={dataSource} columns={columns} />
+        </InfoWrapper>
+      </Container>
+
+      <PostProductModal
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        resForm={resForm}
+      >
+        <AddInfoForm formRef={formRef} setModalOpen={setModalOpen} />
+      </PostProductModal>
     </>
   );
 };
