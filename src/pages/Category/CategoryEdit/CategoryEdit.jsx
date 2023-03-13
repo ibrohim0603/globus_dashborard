fcf6bc18172d103d20151ced5c1f9fa363bdd0a9
bakhodirabdullayev <bahodirabdullayev0556@gmail.com>
2023-03-13 19:49:38 +0500

    nimalardir

--- src/pages/Category/CategoryEdit/CategoryEdit.jsx
+++ src/pages/Category/CategoryEdit/CategoryEdit.jsx
@@ -7,38 +7,28 @@ import { QueryContext } from "../../../App";
 // import CategoryEdit from "../CategoryEdit/CategoryEdit";
 
 const CategoryEdit = ({ editRef, id, setModalOpen }) => {
-  const [photoId, setPhotoId] = useState(null);
+  // const [photoId, setPhotoId] = useState(null);
   const { queryClient } = useContext(QueryContext);
   const editMutate = useEditData("/category");
-  // const [fields, setFields] = useState(null);
 
   const getFields = useGetData(["categories", id], `/category/${id}`);
   const { data } = getFields;
-  // useEffect(() => {
-  //   setFields([
-  //     {
-  //       name: ["name_Uz"],
-  //       value: data?.name_Uz,
-  //     },
-  //     {
-  //       name: ["name_Ru"],
-  //       value: data?.name_Ru,
-  //     },
-  //     {
-  //       name: ["name_En"],
-  //       value: data?.name_En,
-  //     },
-  //   ]);
-  // }, [getFields]);
 
   const onFinish = (values) => {
+    queryClient.invalidateQueries({ queryKey: ["categories", id] });
     editMutate.mutate(
-      { value: { ...values, photoId: photoId || data?.photoId }, id },
+      {
+        value: {
+          ...values,
+          photoId: values?.photoId?.file?.response?.id || data?.photoId,
+        },
+        id,
+      },
       {
         onSuccess: () => {
+          setModalOpen(false);
           queryClient.invalidateQueries({ queryKey: ["categories"] });
           editRef.current.resetFields();
-          setModalOpen(false);
         },
       }
     );
@@ -58,7 +48,7 @@ const CategoryEdit = ({ editRef, id, setModalOpen }) => {
         console.log(info.file, info.fileList);
       }
       if (info.file.status === "done") {
-        setPhotoId(info.file.response.id);
+        // setPhotoId(info.file.response.id);
         message.success(`${info.file.name} file uploaded successfully`);
       } else if (info.file.status === "error") {
         message.error(`${info.file.name} file upload failed.`);
@@ -67,7 +57,7 @@ const CategoryEdit = ({ editRef, id, setModalOpen }) => {
   };
   return (
     <>
-      {getFields.isLoading ? (
+      {editMutate.isLoading ? (
         "Loading"
       ) : (
         <Form
@@ -75,7 +65,6 @@ const CategoryEdit = ({ editRef, id, setModalOpen }) => {
           name="basic"
           labelCol={{ span: 12 }}
           wrapperCol={{ span: 24 }}
-          style={{ maxWidth: 600 }}
           initialValues={{ remember: true }}
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
@@ -94,9 +83,6 @@ const CategoryEdit = ({ editRef, id, setModalOpen }) => {
               value: data?.name_En,
             },
           ]}
-          // onFieldsChange={(_, allFields) => {
-          //   setFields(allFields);
-          // }}
         >
           <Form.Item
             // label="Name_Uz"
@@ -120,14 +106,16 @@ const CategoryEdit = ({ editRef, id, setModalOpen }) => {
             <Input placeholder="Name_En" />
           </Form.Item>
           <div style={{ display: "flex", gap: 20 }}>
-            <Upload {...props}>
-              <Button
-                style={{ display: "flex", alignItems: "center", gap: 10 }}
-                icon={<AiOutlineCloudUpload style={{ fontSize: 20 }} />}
-              >
-                Click to Upload
-              </Button>
-            </Upload>
+            <Form.Item name="photoId">
+              <Upload {...props}>
+                <Button
+                  style={{ display: "flex", alignItems: "center", gap: 10 }}
+                  icon={<AiOutlineCloudUpload style={{ fontSize: 20 }} />}
+                >
+                  Click to Upload
+                </Button>
+              </Upload>
+            </Form.Item>
             <Button
               type="primary"
               htmlType="submit"
